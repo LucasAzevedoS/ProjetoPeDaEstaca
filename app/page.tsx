f5592ad7c976362058ab687d1c0c7fac21ef3a34
Lucas Azevedo de Souza <lucas.a.souza@hotmail.com>
2024-06-18 20:30:28 -0300

    Criacao do carousel, falta estilizar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 // app/page.tsx
 'use client'
+import { register } from 'swiper/element/bundle'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
 import { Link } from '@chakra-ui/next-js'
 import { Box, flexbox } from '@chakra-ui/react'
 import { Flex, Spacer } from '@chakra-ui/react'
@@ -8,8 +15,20 @@ import { Button, ButtonGroup } from '@chakra-ui/react'
 import { Image } from '@chakra-ui/react'
 import './globals.css'
 
+register();
 
-
+const data = [
+  { id: '1', image: './img1.jfif' },
+  { id: '2', image: './img2.jfif' },
+  { id: '3', image: './img3.jfif' },
+  { id: '4', image: './img4.jfif' },
+  { id: '5', image: './img5.jfif' },
+  { id: '6', image: './img6.jfif' },
+  { id: '7', image: './img7.jfif' },
+  { id: '8', image: './img8.jfif' },
+  { id: '9', image: './img9.jfif' },
+  { id: '10', image: './img10.jfif' },
+]
 export default function Page() {
 
   return (
@@ -27,19 +46,19 @@ export default function Page() {
                   <Link href="#objetivo" p='4' textColor='white'>Objetivo</Link>
                 </li>
                 <li>
-                  <Link href="/about" p='4'textColor='white'>Teste</Link>
+                  <Link href="/about" p='4' textColor='white'>Teste</Link>
                 </li>
                 <li>
-                  <Link href="/about" p='4'textColor='white'>Teste</Link>
+                  <Link href="/about" p='4' textColor='white'>Teste</Link>
                 </li>
                 <li>
-                  <Link href="/about" p='4'textColor='white'>Teste</Link>
+                  <Link href="/about" p='4' textColor='white'>Teste</Link>
                 </li>
                 <li>
-                  <Link href="/about" p='4'textColor='white'>Teste</Link>
+                  <Link href="/about" p='4' textColor='white'>Teste</Link>
                 </li>
                 <li>
-                  <Link href="/about" p='4'textColor='white'>Teste</Link>
+                  <Link href="/about" p='4' textColor='white'>Teste</Link>
                 </li>
               </Box>
             </ul>
@@ -48,41 +67,44 @@ export default function Page() {
         </Box>
       </header>
       <Box display='flex' justifyContent='center' p='2rem' backgroundColor='#343D3F'>
-      <Image 
-            boxSize='500px'
-            height='100%'
-            objectFit='cover'
-            src='./estradaArvore.jpg'
-            alt='Dan Abramov'
-        
+        <Image
+          boxSize='500px'
+          height='100%'
+          objectFit='cover'
+          src='./estradaArvore.jpg'
+          alt='Dan Abramov'
+
         />
 
         <Text fontSize='50px' ml='3rem' mt='12rem' fontFamily='roboto' textColor='white' justifyContent='center' textAlign='left'>
 
           Plantar mudas de árvores nativas  <br />no Pé da Estaca ao longo das cercas <br /> as  margens das estradas rurais do nosso município.
-          <Box display='flex' justifyContent='center'><Button variant='outline' colorScheme='green' border='1px'   width='100%'>Junte-se a nós</Button></Box>
-        </Text> 
+          <Box display='flex' justifyContent='center'><Button variant='outline' colorScheme='green' border='1px' width='100%'>Junte-se a nós</Button></Box>
+        </Text>
       </Box>
-      
+
 
 
 
       <Box bg='#343D3F' >
-          <Text  fontFamily='roboto' fontSize='1.5rem' color='white' textAlign='justify' mr='4rem' ml='6rem'  letterSpacing={3} >
-            O projeto Pé da Estaca tem como objetivo desenvolver consciência critica social e ambiental do sistema que interliga a sociedade com interesse comum.<br></br>
-            Para isso esse projeto ecológico Pé da Estaca visa plantar arvores nativas no pé da estaca a cada 50 ou 100 metros de distância as margens das estradas rurais e divisas em sítios e fazenda da região tendo como meta contribuir para o bem-estar animal, redução de gastos aos produtores, diminuição do aquecimento global, conservação ambiental e etc. no que resulta em qualidade de vida das pessoas, somando forças para preservar o nosso bem mais precioso.
-            O projeto piloto foi desenvolvido na fazenda União e no Sitio Cahoeirinha de São Francisco na cidade de Visconde do Rio Branco Estado de Minas Gerais ao todo foram plantadas 200 mudas de arvores de Ipês branco, amarelo e roxo, Angico, Acácia, e árvores frutíferas.
-          </Text>
+        <Text fontFamily='roboto' fontSize='1.5rem' color='white' textAlign='justify' mr='4rem' ml='6rem' letterSpacing={3} >
+          O projeto Pé da Estaca tem como objetivo desenvolver consciência critica social e ambiental do sistema que interliga a sociedade com interesse comum.<br></br>
+          Para isso esse projeto ecológico Pé da Estaca visa plantar arvores nativas no pé da estaca a cada 50 ou 100 metros de distância as margens das estradas rurais e divisas em sítios e fazenda da região tendo como meta contribuir para o bem-estar animal, redução de gastos aos produtores, diminuição do aquecimento global, conservação ambiental e etc. no que resulta em qualidade de vida das pessoas, somando forças para preservar o nosso bem mais precioso.
+          O projeto piloto foi desenvolvido na fazenda União e no Sitio Cahoeirinha de São Francisco na cidade de Visconde do Rio Branco Estado de Minas Gerais ao todo foram plantadas 200 mudas de arvores de Ipês branco, amarelo e roxo, Angico, Acácia, e árvores frutíferas.
+        </Text>
       </Box>
+
+
+
 
       <Flex bg='#343D3F' alignItems='center' justifyContent='space-around' >
         <Box p='1rem' mt='2rem'>
 
-          <Text fontFamily='roboto' textColor='white'  fontSize='1.1rem' as='cite'  textAlign='left' mr='2rem' ml='2rem'>
+          <Text fontFamily='roboto' textColor='white' fontSize='1.1rem' as='cite' textAlign='left' mr='2rem' ml='2rem'>
             “Comece fazendo o que é necessário, depois o que é possível, e de repente você estará fazendo o impossível...” São Francisco de Assis.<br></br>
           </Text>
-          <Text fontFamily='roboto' textColor='white' fontSize='1.1rem' as='cite'  textAlign='left' mr='2rem' ml='2rem'>
-          ‎ Que São Francisco nos abençoe, amém!
+          <Text fontFamily='roboto' textColor='white' fontSize='1.1rem' as='cite' textAlign='left' mr='2rem' ml='2rem'>
+            ‎ Que São Francisco nos abençoe, amém!
             O Meio Ambiente, agradece!
           </Text>
 
@@ -92,7 +114,21 @@ export default function Page() {
           <img src="./francisco.jfif" alt="imagem de Sáo Francisco de Assis" />
         </Box>
       </Flex>
-
+      <Swiper
+          slidesPerView={4}
+          pagination={{ clickable: true }}
+          navigation
+        >
+          {data.map((item) => (
+            <SwiperSlide key={item.id}>
+              <img
+                src={item.image}
+                alt='slider'
+                className='slider-item'
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
 
 
     </>
